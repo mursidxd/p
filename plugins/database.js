3bac1b402110bab993d11234ce813be64a4ff1f8
мυʀѕι∂-χмℓ <110702247+mursidxd@users.noreply.github.com>
2022-09-25 06:09:42 +0700

    Update database.js

--- plugins/database.js
+++ plugins/database.js
@@ -1,26 +1,11 @@
-let fs = require('fs')
-let handler  = async (m, { conn, text }) => {
-m.reply('Tunggu Sebentar, Proses Getting File database.json')
-let db = fs.readFileSync('./database.json')
-conn.sendFile(m.chat, db, 'database.json', m)
+let handler = async (m, { usedPrefix, command, conn, text }) => {
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+    let kon = `*Database saat ini ${totalreg} user*\n*Registrasi saat ini ${rtotalreg} user*`
+    await conn.sendButtonLoc(m.chat, await(await require('node-fetch')(fla + `${command}`)).buffer(), kon, wm, 'Menu', usedPrefix + 'menu', m)
 }
-handler.help = ['getdb','getdatabase'].map(v => v + ' <teks>')
-handler.tags = ['owner']
-handler.command = /^(db|getdb)$/i
-handler.owner = true
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.help = ['user']
+handler.tags = ['info']
+handler.command = /^(pengguna|(jumlah)?database|user)$/i
 
 module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
