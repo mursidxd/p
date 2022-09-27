cb67ea7d3d7effa0a472da8d324384be2583e9a4
мυʀѕι∂-χмℓ <110702247+mursidxd@users.noreply.github.com>
2022-09-28 05:22:34 +0700

    Update main-thanks.js

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://database.tioclkp02.repl.co/1240fb4a4d600fb02355b.png', m, { packname: "Sama Sama by", author: "Tio" })
+conn.sendImageAsSticker(m.chat, 'https://database.tioclkp02.repl.co/1240fb4a4d600fb02355b.png', m, { packname: "Instagram : @mursid.st", author: "мυʀѕι∂ вσт-χмℓ" })
 }
 
-handler.customPrefix = /^(Terima Kasih|Thanks|tq)$/i
+handler.customPrefix = /^(Terima Kasih|Thanks|tq|makasih|makasi|thx|thanks)$/i
 handler.command = new RegExp
 
 module.exports = handler
